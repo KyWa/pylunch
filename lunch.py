83b51fd93be56fddf39d7887a7ee227cbd51fe46
kyle <reformed.rev@gmail.com>
2016-04-11 20:28:15 -0500

    added exception for those without pyfiglet lib

--- lunch.py
+++ lunch.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python2
 
 from random import randrange
-from pyfiglet import figlet_format
+try:
+    from pyfiglet import figlet_format
+    def banner(x):
+        print "[*]----------------------------[*]"
+        print figlet_format(x)
+        print "[*]----------------------------[*]"
 
-def banner(x):
-    print "[*]----------------------------[*]"
-    print figlet_format(x)
-    print "[*]----------------------------[*]"
+except ImportError:
+    def banner(x):
+        print "[*]----------------------------[*]"
+        print x
+        print "[*]----------------------------[*]"
+
+
 def lunch():
     choices = {1:"PDQ",2:"Torchys",3:"MOD",4:"Freeibrds",5:"Popeyes",6:"My Pizzaria",7:"Whataburger",8:"Taco Bell",9:"Arbys",10:"Smash Burger",11:"Potbellys"}
     x = randrange(1,12)
