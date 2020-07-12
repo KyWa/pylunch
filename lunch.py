d3c6c855e40cfac586cb5c639b08ec71c367fc6d
Kyle Walker <unixislife@gmail.com>
2020-07-12 12:00:10 -0500

    move to python3

--- lunch.py
+++ lunch.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
 from random import randrange
 #assumes people have pyfiglet libs
 try:
     from pyfiglet import figlet_format
     def banner(x):
-        print "[*]----------------------------[*]"
-        print figlet_format(x)
-        print "[*]----------------------------[*]"
+        print("[*]----------------------------[*]")
+        print(figlet_format(x))
+        print("[*]----------------------------[*]")
 #if no pyfiglet, just print basic
 except ImportError:
     def banner(x):
-        print "[*]----------------------------[*]"
-        print x
-        print "[*]----------------------------[*]"
+        print("[*]----------------------------[*]")
+        print(x)
+        print("[*]----------------------------[*]")
 
 def lunch():
     #this is a dic, examp: {1:"choice",2:"choice2"}
-    choices = {1:"PDQ",2:"Torchys",3:"MOD",4:"Freebirds",5:"Popeyes",6:"My Pizzaria",7:"Whataburger",8:"Taco Bell",9:"Arbys",10:"Smash Burger",11:"Potbellys"} 
+    choices = {1:"Whataburger",2:"Jack in the Box",3:"Jason's Deli",4:"Jimmy Johns"}
     #makes the choice based on randrange adding 1 (to avoid a 0 result)
     banner(choices[randrange(len(choices))+1])
 
