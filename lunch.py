6043010dc5b0e6286aaa6a7ac7851821ecdcfd48
Kyle Walker <unixislife@gmail.com>
2020-09-26 02:03:30 -0500

    added Dockerfile and updated choices

--- lunch.py
+++ lunch.py
@@ -1,22 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 from random import randrange
 #assumes people have pyfiglet libs
-try:
-    from pyfiglet import figlet_format
-    def banner(x):
-        print("[*]----------------------------[*]")
-        print(figlet_format(x))
-        print("[*]----------------------------[*]")
-#if no pyfiglet, just print basic
-except ImportError:
-    def banner(x):
-        print("[*]----------------------------[*]")
-        print(x)
-        print("[*]----------------------------[*]")
+def banner(x):
+    print("[*]----------------------------[*]")
+    print(x)
+    print("[*]----------------------------[*]")
 
 def lunch():
     #this is a dic, examp: {1:"choice",2:"choice2"}
-    choices = {1:"Whataburger",2:"Jack in the Box",3:"Jason's Deli",4:"Jimmy Johns"}
+    choices = {1:"McDonalds",2:"Whataburger",3:"Jack in the Box",4:"Jimmy Johns",5:"Five Guys",6:"Chick-fil-a"}
     #makes the choice based on randrange adding 1 (to avoid a 0 result)
     banner(choices[randrange(len(choices))+1])
 
