22def9f1e2244920b64abb0db56d8c0a65ef8e95
Kyle Walker <unixislife@gmail.com>
2021-08-19 09:33:56 -0500

    option cleanup

--- lunch.py
+++ lunch.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python2
 from random import randrange
-#assumes people have pyfiglet libs
 def banner(x):
     print("[*]----------------------------[*]")
     print(x)
@@ -8,7 +7,7 @@ def banner(x):
 
 def lunch():
     #this is a dic, examp: {1:"choice",2:"choice2"}
-    choices = {1:"McDonalds",2:"Whataburger",3:"Jack in the Box",4:"Jimmy Johns",5:"Five Guys",6:"Chick-fil-a"}
+    choices = {1:"Robertos",2:"Hyderabad",3:"Crust"}
     #makes the choice based on randrange adding 1 (to avoid a 0 result)
     banner(choices[randrange(len(choices))+1])
 
