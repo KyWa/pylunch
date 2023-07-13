f71499047cbf7fdd9a2b2336417b046cac637da5
Kyle Walker <kylwalke@Kyles-MacBook-Pro.local>
2023-07-13 11:09:36 -0500

    fixing

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app
 from flask import render_template
+import lunch
 
 @app.route('/')
 @app.route('/index')
 def index():
-    choices = lunch()
+    choices = picker()
     return render_template('index.html', title="yeet", choices=choices)
