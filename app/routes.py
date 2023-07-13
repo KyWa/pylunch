ac2843c1cd8aa3baf23d9da7f5ae6064cc6f88e7
Kyle Walker <kylwalke@Kyles-MacBook-Pro.local>
2023-07-13 11:25:08 -0500

    final test

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
+from random import randrange
 from flask import render_template
-from lunch import picker
 
 @app.route('/')
 @app.route('/index')
 def index():
-    choices = picker()
-    return render_template('index.html', title="yeet", choices=choices)
+    choices = {1:"Robertos",2:"Hyderabad",3:"Spring Food Park",4:"Olive Oil",5:"Freebirds",6:"Torchys",7:"Chick-fil-a"}
+    choice = choices[randrange(len(choices))+1]
+    print(choice)
+    return render_template('index.html', title="yeet", choice=choice)
